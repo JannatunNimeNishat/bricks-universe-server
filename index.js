2548f066e3073f8c5bbd69ddfdf4bfccf51548fd
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-19 01:39:25 +0600

    api - get toys added by a specific seller api

--- index.js
+++ index.js
@@ -31,49 +31,81 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
-  const toysCollection = client.db('bricksUniverseDB').collection('toys')
+
+    const toysCollection = client.db('bricksUniverseDB').collection('toys')
     const categoriesCollection = client.db('bricksUniverseDB').collection('categories')
 
 
 
     //categories CRUD
     //READ all categories
-    app.get('/categories', async(req,res)=>{
+    app.get('/categories', async (req, res) => {
       const result = await categoriesCollection.find().toArray();
       res.send(result)
     })
 
 
 
-  //Toys CRUD
+    //Toys CRUD
 
-  
-  //read a specific category toy
-  app.get('/toys', async (req,res)=>{
-    // console.log(req.query);
-    let query = {}
-    if(req.query.sub_category){
-      query = {sub_category:req.query.sub_category}
-    }
-    const result = await toysCollection.find(query).toArray()
-    // console.log(result);
-    res.send(result)
-  })
-  
-  //READ get all toys
-  app.get('/toys', async(req,res)=>{
-    const result = await toysCollection.find().toArray()
-    res.send(result)
-  })
 
-  //READ a specific toy by id
-  app.get('/toy/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await toysCollection.findOne(query)
-    res.send(result)
-  })
+    //read a specific category toy
+    app.get('/toys', async (req, res) => {
+      // console.log(req.query);
+      let query = {}
+      if (req.query.sub_category) {
+        query = { sub_category: req.query.sub_category }
+      }
+      const result = await toysCollection.find(query).toArray()
+      // console.log(result);
+      res.send(result)
+    })
+
+    //READ a specific toy by id
+    app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.findOne(query)
+      res.send(result)
+    })
+
+    //READ get all toys
+    app.get('/allToys', async (req, res) => {
+      console.log('reached');
+      const query = {}
+      const cursor = toysCollection.find(query).limit(20)
+      const result = await cursor.toArray()
+      console.log(result);
+      res.send(result)
+    })
+
+    //get toys added by a specific seller
+    app.get('/userAddedToys', async(req,res)=>{
+      
+      let query={};
+      console.log(req.query.seller_email);
+      if(req.query.seller_email){
+        query = {seller_email : req.query.seller_email}
+      }
+      const result = await toysCollection.find(query).toArray()
+     
+      res.send(result)
+
+    })
+
+
+
+    //CREATE a toy
+
+    app.post('/toy', async (req, res) => {
+      const toy = req.body;
+      const result = await toysCollection.insertOne(toy)
+      res.send(result)
+    })
+
+    
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -92,12 +124,12 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
-    res.send('bricks universe is running');
+app.get('/', (req, res) => {
+  res.send('bricks universe is running');
 })
 
-app.listen(port, ()=>{
-    console.log(`bricks universe is running at port: ${port}`);
+app.listen(port, () => {
+  console.log(`bricks universe is running at port: ${port}`);
 })
 
 
